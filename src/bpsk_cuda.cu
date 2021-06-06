cf1e951a8435a5d9c43137ad318abfa95e2d0feb
mgoldyn <mgoldyn@o2.pl>
2021-06-06 18:16:07 +0200

    working bpsk_cuda

--- src/bpsk_cuda.cu
+++ src/bpsk_cuda.cu
@@ -44,7 +44,7 @@ set_phase_shift_cuda(int32_t* phase_offset,
         int32_t sig_idx = 0;
         for(; sig_idx < n_cos_samples; ++sig_idx)
         {
-            modulated_signal_ptr[sig_idx] = signal_data[phase_offset[bit_idx / N_MAX_DEGREE] + sig_idx];
+            modulated_signal_ptr[sig_idx] = signal_data[phase_offset[bit_idx] + sig_idx];
         }
 //        modulated_signal[sig_idx] = signal_data[scaled_phase_shift + sig_idx];
     }
@@ -60,8 +60,9 @@ void modulate_bpsk_cuda(int32_t n_cos_samples,
     float* d_signal_data;
     int32_t* d_bit_stream;
     int32_t* d_phase_offset;
+    int32_t n_elem = n_bits < N_CUDA_ELEM ? n_bits : N_CUDA_ELEM;
 
-    cudaMalloc((void**)&d_modulated_signal, sizeof(float) * n_cos_samples * N_CUDA_ELEM);
+    cudaMalloc((void**)&d_modulated_signal, sizeof(float) * n_cos_samples * n_elem);
     cudaMalloc((void**)&d_signal_data, sizeof(float) * n_cos_samples * 2);
     cudaMalloc((void**)&d_bit_stream, sizeof(int32_t) * n_bits);
     cudaMalloc((void**)&d_phase_offset, sizeof(int32_t) * n_bits);
@@ -69,28 +70,28 @@ void modulate_bpsk_cuda(int32_t n_cos_samples,
     cudaMemcpy(d_signal_data, signal_data, sizeof(float) * n_cos_samples * 2, cudaMemcpyHostToDevice);
 
     int threadsPerBlock = 16;
-//    int blocksPerGrid = (n_bits * n_cos_samples + threadsPerBlock - 1) / threadsPerBlock;
     int blocksPerGrid = (N_CUDA_ELEM + threadsPerBlock - 1) / threadsPerBlock;
-//    printf("mgoldyn blocks = %d", blocksPerGrid);
 
     int32_t bit_idx = 0;
     for(; bit_idx < n_bits; bit_idx += N_CUDA_ELEM)
     {
+        int32_t n_cuda_bits = n_bits < N_CUDA_ELEM ? n_bits : bit_idx + N_CUDA_ELEM > n_bits ? n_bits - bit_idx: N_CUDA_ELEM;
         set_phase_offset_cuda<<<blocksPerGrid, threadsPerBlock>>>(d_bit_stream,
-                                                                  n_bits - (bit_idx * N_CUDA_ELEM),
+                                                                  n_cuda_bits,
                                                                   &d_phase_offset[bit_idx]);
     }
-
     for(bit_idx = 0; bit_idx < n_bits; bit_idx += N_CUDA_ELEM)
     {
-        int32_t n_cuda_bits = n_bits - (bit_idx * N_CUDA_ELEM);
-        set_phase_shift_cuda<<<blocksPerGrid, threadsPerBlock>>>(&d_phase_offset[bit_idx], n_cos_samples, n_cuda_bits, d_signal_data, d_modulated_signal);
-        cudaMemcpy(&modulated_signal[bit_idx * n_cos_samples], d_modulated_signal, sizeof(float) * n_cos_samples * n_cuda_bits, cudaMemcpyDeviceToHost);
-
-//        for(int32_t i = 0; i < 360; ++i)
-//        {
-//            printf("mgoldyn out[%d] = %e\n", i, modulated_signal[i]);
-//        }
+        int32_t n_cuda_bits = n_bits < N_CUDA_ELEM ? n_bits : bit_idx + N_CUDA_ELEM > n_bits ? n_bits - bit_idx: N_CUDA_ELEM;
+        set_phase_shift_cuda<<<blocksPerGrid, threadsPerBlock>>>(&d_phase_offset[bit_idx],
+                                                                 n_cos_samples,
+                                                                 n_cuda_bits,
+                                                                 d_signal_data,
+                                                                 d_modulated_signal);
+        cudaMemcpy(&modulated_signal[bit_idx * n_cos_samples],
+                   d_modulated_signal,
+                   sizeof(float) * n_cos_samples * n_cuda_bits,
+                   cudaMemcpyDeviceToHost);
     }
     cudaFree((void*)d_modulated_signal);
     cudaFree((void*)d_signal_data);
